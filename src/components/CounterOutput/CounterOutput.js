49fdd138ef88ee590fc16de51cf27a3054226f24
zohredk <zohrehdk2000@gmail.com>
2024-11-04 21:48:56 +1100

    .

--- src/components/CounterOutput/CounterOutput.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./CounterOutput";
-
-const CounterOutput = (props) => (
-  <div className="counter-output"> Count: {props.value}</div>
-);
-
-export default CounterOutput;
